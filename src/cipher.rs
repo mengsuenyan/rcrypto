3474d28043e1f9b7c83e45454a1e7ed773f207ef
mengsuenyan <mengsuenyan@163.com>
2020-09-22 14:34:35 +0800

    [ADD] add the trait Digest for the message digest algorithm used in the cryptography

--- src/cipher.rs
+++ src/cipher.rs
@@ -2,6 +2,7 @@
 
 use crate::crypto_err::CryptoError;
 
+/// A trait for cryptography algorithms
 pub trait Cipher {
     /// The cryptography algorithm used data block size(in bytes) for plaintext, `None` means that there is
     /// no requirement for the data block size.
@@ -14,4 +15,19 @@ pub trait Cipher {
     /// To decrypt the `cipher_block` and output the decrypted data `dst`, the length in bytes of
     /// the decrypted data will return if decrypt success, other `CryptoError` returned.
     fn decrypt(&self, dst: &mut Vec<u8>, cipher_block: &[u8]) -> Result<usize, CryptoError>;
+}
+
+/// A trait for message digest algorithm used in the cryptography
+pub trait Digest {
+    /// block size in bytes
+    fn block_size(&self) -> Option<usize>;
+    
+    /// write data to the Digester
+    fn write(&mut self, data: &[u8]);
+    
+    /// finished the digest and output to the `digest`
+    fn finish(&mut self, digest: &mut Vec<u8>);
+    
+    /// reset internal state of the Digester to the init state
+    fn reset(&mut self);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 extern crate rmath;
 
 mod cipher;
-pub use cipher::Cipher;
+pub use cipher::{Cipher, Digest};
 
 mod crypto_err;
 pub use crypto_err::{CryptoErrorKind, CryptoError};
@@ -12,3 +12,5 @@ pub use aes::AES;
 
 mod des;
 pub use des::DES;
+
+mod md5;
